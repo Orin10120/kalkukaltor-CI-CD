4fc6df3057a4914aa8922ec525d2bb1cd76813d1
Rindang Bani Isyan <109974656+Orin10120@users.noreply.github.com>
2025-06-13 17:23:02 +0700

    Latihan: Menambahkan potensi code smell untuk SonarCloud

--- script.js
+++ script.js
@@ -49,3 +49,11 @@ function calculateResult() {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { performCalculation };
 }
+
+function clearDisplay() {
+
+display.value = '';
+
+let unusedVariableForDemo = "hello"; // Variabel tidak digunakan
+
+}
